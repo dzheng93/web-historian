d9fce656268cc4ded7782f1d0eb5912541779ef6
dzheng93 <dunstanzheng@gmail.com>
2016-11-21 20:14:24 -0800

    Serve index.html as static resource and begin other REST routing

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,6 +14,16 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  var assetPath;
+
+  if (asset === 'index') {
+    assetPath = archive.paths.index;
+    res.setHeader('Content-Type', 'text/html');
+  }
+
+  fs.readFile(assetPath, function(err, data) {
+    callback(err, data);
+  });
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,37 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var statusCode;
+
+
+  if (req.method === 'GET') {
+    statusCode = 200;
+
+    httpHelpers.serveAssets(res, 'index', function(err, data) {
+      res.writeHead(statusCode, httpHelpers.headers);
+      res.end(data);
+    });
+  } else if (req.method === 'POST') {
+    statusCode = 201;
+    req.on('data', function(data) {
+      var url = data.toString('utf8').slice(4);
+
+    
+      res.writeHead(statusCode, httpHelpers.headers);
+
+    });
+  } else {
+    statusCode = 404;
+    res.writeHead(statusCode, httpHelpers.headers);
+    res.end(JSON.stringify(''));
+  }
+
+
+
+  
+
+  // res.end(archive.paths.list);
 };
